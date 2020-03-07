e2b212317196631f59f2e30a5c4d3f522f8d4ae7
Jonas Gabriel <jgalmeida1993@gmail.com>
2020-03-07 16:44:46 -0300

    :construction: Cria app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Hero from "./Hero";
+import Footer from "./Footer";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Hero />
+        <Footer />
+      </div>
+    );
+  }
 }
-
 export default App;
+
+// 9905624fce162ae6ce4177dda41c0302 md5
+
+// 246e196d508fa427176b7bc94ae03637 public
+
+// d06ddd840dadeb41e3bdd4e16de61848cafaad64 private
